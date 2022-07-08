8fe56c76f7c3fa31c3ccbc85f2570b15d316448c
NikolayBondar <nikolai.bondar@gmail.com>
2022-07-08 18:46:40 +0000

    Edit script

--- Lesson7/status.sh
+++ Lesson7/status.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-id=`ps -aux | grep 'apache2 -k start' | awk '$1=$1' | cut -d " " -f2 | head -n 1`
+id=$(ps -aux | grep 'apache2 -k start' | awk '$1=$1' | cut -d " " -f2 | head -n 1)
 
-name=`ps -aux | grep 'apache2 -k start' | awk '$1=$1' | cut -d " " -f11 | head -n 1`
+name=$(ps -aux | grep 'apache2 -k start' | awk '$1=$1' | cut -d " " -f11 | head -n 1)
 
 `kill -15 $id`
 
