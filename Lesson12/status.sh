3951867a12fc1a3e842602c71be37d8bbe71d470
NikolayBondar <nikolai.bondar@gmail.com>
2022-07-30 21:27:02 +0300

    add script

---
+++ Lesson12/status.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+id=$(systemctl status apache2 | grep PID | tr -s ' ' | cut -d ' ' -f4 | tr -d '/n')
+stat=$(systemctl status apache2| grep Active | tr -s " " | cut -d ' ' -f 3)
+
+name=$(systemctl status apache2 | grep 'apache2.service -' | tr -s ' ' | cut -d '-' -f2)
+
+if [ -z $id ] || [ $stat == "inactive" ]
+then
+  echo "Process not found or Inactive"
+else
+
+  `systemctl stop apache2`
+
+  echo "Process $name $id stopped"
+
+  sleep 10
+
+  n=$(ps -p $id | wc -l)
+
+
+  if [ "$n" -gt 1 ]	
+
+  then
+
+    `kill -15 $id`
+  fi
+fi 
+
