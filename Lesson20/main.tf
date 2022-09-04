f5f32b1cfec6c12123913b33b705f87d7206d210
Nikolay Bondar <nikolai.bondar@gmail.com>
2022-09-04 17:34:13 +0300

    edit *.tf

--- Lesson20/main.tf
+++ Lesson20/main.tf
@@ -2,7 +2,7 @@ resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "azure-terraform-test" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
@@ -11,50 +11,38 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "my_terraform_network" {
   name                = "myVnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name = azurerm_resource_group.azure-terraform-test.name
 }
 
 # Create subnet
 resource "azurerm_subnet" "my_terraform_subnet" {
   name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.azure-terraform-test.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.128.0/24"]
 }
 
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "myPublicIP"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name = azurerm_resource_group.azure-terraform-test.name
   allocation_method   = "Dynamic"
 }
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "my_terraform_nsg" {
   name                = "myNetworkSecurityGroup"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+  location            = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name = azurerm_resource_group.azure-terraform-test.name
 }
 
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
   name                = "myNIC"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name = azurerm_resource_group.azure-terraform-test.name
 
   ip_configuration {
     name                          = "my_nic_configuration"
@@ -74,7 +62,7 @@ resource "azurerm_network_interface_security_group_association" "example" {
 resource "random_id" "random_id" {
   keepers = {
     # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
+    resource_group = azurerm_resource_group.azure-terraform-test.name
   }
 
   byte_length = 8
@@ -83,48 +71,37 @@ resource "random_id" "random_id" {
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
   name                     = "diag${random_id.random_id.hex}"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name      = azurerm_resource_group.azure-terraform-test.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# Create (and display) an SSH key
-resource "tls_private_key" "example_ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
 # Create virtual machine
-resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
-  name                  = "TestUnix"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+resource "azurerm_windows_virtual_machine" "my_terraform_vm" {
+  name                  = "Win10Pro"
+  location              = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name   = azurerm_resource_group.azure-terraform-test.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_DS1_v2"
+  size                  = "DS1_v2"
+  admin_username      = "azureuser"
+  admin_password      = "P@$$w0rD"
 
   os_disk {
     name                 = "myOsDisk"
     caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-10"
+    sku       = "rs4-pro-g2"
+    version   = "17134.1246.2001090113"
   }
 
-  computer_name                   = "myvm"
-  admin_username                  = "azureuser"
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = tls_private_key.example_ssh.public_key_openssh
-  }
-
+  computer_name                   = "vmwin10pro"
+  
   boot_diagnostics {
     storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
   }
--- Lesson20/variables.tf
+++ Lesson20/variables.tf
@@ -1,9 +1,9 @@
 variable "resource_group_location" {
-  default     = "eastus"
+  default     = "West Europe"
   description = "Location of the resource group."
 }
 
 variable "resource_group_name_prefix" {
-  default     = "rg"
+  default     = "azure-terraform-test"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
